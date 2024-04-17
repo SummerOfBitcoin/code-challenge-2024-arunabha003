50c0a256503353b9197edf5ed00551cb27546fb9
Arunabha <arunabhadhal04@gmail.com>
2024-04-18 03:57:40 +0530

    ...

--- Block_Header2.py
+++ Block_Header2.py
@@ -7,6 +7,6 @@ def field(data, size):
     return format(data, '0' + str(size * 2) + 'x')
 
 def create_block_header(version,prev_block,Merkle_Root,time,bits,nonce):
- header = reverse_bytes(field(version, 4)) + reverse_bytes(prev_block) + Merkle_Root + reverse_bytes(field(time, 4)) + reverse_bytes(bits)+reverse_bytes(field(nonce, 4))
- print(header)
+ header = reverse_bytes(field(version, 4)) + reverse_bytes(prev_block) + reverse_bytes(Merkle_Root) + reverse_bytes(field(time, 4)) + reverse_bytes(bits)+reverse_bytes(field(nonce, 4))
+ #print(header)
  return header
--- MerkleRoot.py
+++ MerkleRoot.py
@@ -1,51 +1,41 @@
-import os
 import hashlib
+import binascii
+import os
 
-# Hash function used in the merkle root function (and in bitcoin in general)
-def hash256(hex_str):
-    binary = bytes.fromhex(hex_str)
-    hash1 = hashlib.sha256(binary).digest()
-    hash2 = hashlib.sha256(hash1).digest()
-    result = hash2.hex()
-    return result
+def hashIt(firstTxHash, secondTxHash):
+    # Reverse inputs before and after hashing
+    # due to big-endian
+    unhex_reverse_first = binascii.unhexlify(firstTxHash)[::-1]
+    unhex_reverse_second = binascii.unhexlify(secondTxHash)[::-1]
 
-# Reverse the order of bytes (often happens when working with raw bitcoin data)
-def reverse_bytes(data):
-    return ''.join(reversed([data[i:i+2] for i in range(0, len(data), 2)]))
+    concat_inputs = unhex_reverse_first+unhex_reverse_second
+    first_hash_inputs = hashlib.sha256(concat_inputs).digest()
+    final_hash_inputs = hashlib.sha256(first_hash_inputs).digest()
+    # reverse final hash and hex result
+    return binascii.hexlify(final_hash_inputs[::-1])
+ 
+ # Hash pairs of items recursively until a single value is obtained
+def merkleCalculator(hashList):
+    if len(hashList) == 1:
+        return hashList[0]
+    newHashList = []
+    # Process pairs. For odd length, the last is skipped
+    for i in range(0, len(hashList)-1, 2):
+        newHashList.append(hashIt(hashList[i], hashList[i+1]))
+    if len(hashList) % 2 == 1: # odd, hash last item twice
+        newHashList.append(hashIt(hashList[-1], hashList[-1]))
+    return merkleCalculator(newHashList)
 
-def merkle_root(txids):
-    # Exit Condition: Stop recursion when we have one hash result left
-    if len(txids) == 1:
-        # Convert the result to a string and return it
-        return txids[0]
 
-    # Keep an array of results
-    result = []
 
-    # 1. Split up array of hashes into pairs
-    for i in range(0, len(txids), 2):
-        one = txids[i]
-        two = txids[i+1] if i+1 < len(txids) else one  # If there's only one left, use it again
-
-        # 2. Concatenate each pair
-        concat = one + two
-
-        # 3. Hash the concatenated pair and add to results array
-        result.append(hash256(concat))
-
-    # Recursion: Do the same thing again for these results
-    return merkle_root(result)
-
-# Function to read txids from the "output.txt" file and compute the merkle root
+# Transaction Hashes of block #100000
 def compute_merkle_root(file_path):
     with open(file_path, 'r') as file:
         txids = [line.strip() for line in file]
 
-    # TXIDs must be in natural byte order when creating the merkle root
-    #reverse_bytes(txids)
 
-    # Compute the merkle root
-    #print(merkle_root(txids))
-    return merkle_root(txids)
+    CalculatedMerkleRoot = str(merkleCalculator(txids), 'utf-8')
+    #print(CalculatedMerkleRoot)
 
-
+    return CalculatedMerkleRoot
+   
